6942e61f1d3d62a0a86ddb1b55d73bd3f8febb03
Hackbright-student <info@hackbrightacademy.com>
2016-01-19 18:00:41 -0800

    mad libs commit

---
+++ madlibs.py
@@ -0,0 +1,60 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Save hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user."""
+
+    player = request.args.get("person") #getting the name entered in the previous form via this, choosing a random compliment, and passing both the person variable and the compliment to the function
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+@app.route('/game')
+def show_game_form():
+    print "HELLLOOOOO!!!!!!!!!!!!!####################################################"
+    #request user response
+    user_response = request.args.get("question")
+    print user_response
+
+    if user_response == "no":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+    # return "HII"
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
+
+# @app.route('/madlib')
+# def ():
